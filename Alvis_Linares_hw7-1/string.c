4a7c6ed5a80254699c7c0d4dccf88fa5ca131dff
alvise30 <elkinalvisnarvaez@gmail.com>
2014-11-07 19:13:31 -0500

    Creando Carpetas

---
+++ Alvis_Linares_hw7-1/string.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define L 100
+#define T 40
+#define T_MAX 120
+
+int main(){
+
+ double rho;
+ double c = sqrt(T/r0);
+
+  File *f = fopen("string_rho.dat", "w");
+
+
+
+
+}
